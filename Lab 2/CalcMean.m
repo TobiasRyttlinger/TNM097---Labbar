5cfa3db74a8c2a2b9981a42df0f415cf724060a2
Tobias Ryttlinger <tosryr@gmail.com>
2020-01-29 14:28:07 +0100

    Lab 2 done

--- Lab 2/CalcMean.m
+++ Lab 2/CalcMean.m
@@ -1,8 +1,9 @@
 function [maxVal,meanVal] = CalcMean(Calibrated,Reference)
-[lc,ac,bc] = xyz2lab(Calibrated(1,:),Calibrated(2,:),Calibrated(2,:));
-[lr,ar,br] = xyz2lab(Reference(1,:),Reference(2,:),Reference(2,:));
+[lc,ac,bc] = xyz2lab(Calibrated(1,:),Calibrated(2,:),Calibrated(3,:));
+[lr,ar,br] = xyz2lab(Reference(1,:),Reference(2,:),Reference(3,:));
 diff = sqrt((lc-lr).^2 +(ac-ar).^2 +(bc-br).^2);
 maxVal = max(diff)
 meanVal = mean(diff)
+diff
 end
 
